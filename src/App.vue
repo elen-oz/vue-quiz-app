581eab9232a611ca43cfefa3a07112549ad62ca6
elen-oz <ozerova.lenka@gmail.com>
2024-09-09 21:47:48 +0200

    feat: add card component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
+<script setup>
+import q from "./data/quizes.json"
+import {ref, watch} from "vue"
+import Card from "./components/Card.vue"
+
+const quizes = ref(q)
+const search = ref("")
+
+console.log('--- quizes',quizes)
+
+watch(search, () => {
+  quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+})
+
+</script>
+
 <template>
   <div class="container">
     <header>
       <h1>Quizes</h1>
-      <input type="text" placeholder="Search...">
+      <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <div class="card">
-        <img src="https://i.ytimg.com/vi/Kp2bYWRQylk/maxresdefault.jpg" alt="">
-        <div class="card-text">
-          <h2>Math</h2>
-          <p>15 questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
@@ -47,31 +57,5 @@ header input {
   margin-top: 40px;
 }
 
-/* CARD */
 
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0
-}
-
-.card .card-text {
-  padding: 0 5px
-}
-
-.card .card-text h2 {
-  font-weight: bold;
-}
 </style>
-<script setup lang="ts">
-</script>
